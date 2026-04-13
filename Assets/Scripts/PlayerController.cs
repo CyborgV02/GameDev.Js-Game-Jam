95c8c500df9670a3d2c453351ed483d9626f4f01
Cyborg <ahmedkamalv2@gmail.com>
2026-04-13 21:22:03 +0200

    Added Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float moveSpeed=5.0f;
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
+    void Awake()
     {
-        
+        playerRb=GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        playerRb.velocity=moveInput*moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput=context.ReadValue<Vector2>();
     }
 }
