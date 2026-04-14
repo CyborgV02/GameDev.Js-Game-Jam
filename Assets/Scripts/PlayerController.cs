7ba2258915ccc1f28a0ce64dac22b8760cd3a31b
Ahmed Elwakil <ahmedbasha243@gmail.com>
2026-04-14 22:52:33 +0200

    Combat UI Prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,6 @@ public class PlayerController : MonoBehaviour
     private MainCharacter playerCharacter;
     public MainCharacter Character { get { return playerCharacter; } }
     
-
     void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
