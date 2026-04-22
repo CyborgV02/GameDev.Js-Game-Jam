ee9c8feee878a31ebe555c173350a70f589ad9bb
Ahmed Elwakil <35235801+DeletedArch@users.noreply.github.com>
2026-04-22 22:57:30 +0200

    Restore Scene

--- Assets/Scripts/Minigames/NodeTransfer.cs
+++ Assets/Scripts/Minigames/NodeTransfer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NodeTransfer : MonoBehaviour
+public class NodeTransfer : MonoBehaviour, IMinigame
 {
     [SerializeField] private GameObject battleSquare;
     [SerializeField] private GameObject nodePrefab;
@@ -11,6 +11,15 @@ public class NodeTransfer : MonoBehaviour
     [SerializeField] private Node GoalNode;
     [SerializeField] private Node objectiveNode;
     [SerializeField] private Node enemyNode;
+    [SerializeField] private float _duration = 5f;
+    [SerializeField] private int _allyDamage = 0;
+    [SerializeField] private int _enemyDamage = 0;
+    private string _name = "Node Transfer";
+
+    public string name => _name;
+    public float duration => _duration;
+    public int allyDamage => _allyDamage;
+    public int enemyDamage => _enemyDamage;
 
     void Start()
     {
@@ -24,5 +33,35 @@ public class NodeTransfer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartMinigame()
+    {
+        // TODO: Implement StartMinigame logic
+    }
+
+    public void EndMinigame()
+    {
+        // TODO: Implement EndMinigame logic
+    }
+
+    public void UpdateMinigame(float deltaTime)
+    {
+        // TODO: Implement UpdateMinigame logic
+    }
+
+    public void HandleInput()
+    {
+        // TODO: Implement HandleInput logic
+    }
+
+    public void DamageAllies(Character[] allies)
+    {
+        // TODO: Implement DamageAllies logic
+    }
+
+    public void DamageEnemies(Enemy[] enemies)
+    {
+        // TODO: Implement DamageEnemies logic
     }
 }
