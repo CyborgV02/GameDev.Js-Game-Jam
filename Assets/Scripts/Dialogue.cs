40ac0e18d729410de9646c7981f3901dad8ddf97
Cyborg <ahmedkamalv2@gmail.com>
2026-04-24 03:28:41 +0200

    Added Dialogue scriptable object

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue")]
+public class Dialogue : ScriptableObject {
+
+    public string recordName;
+    public string[] dialogueLines;
+    public float typingSpeed=0.5f;
+    public AudioClip voiceSound;
+    public float voicePitch=1.0f;
+    public bool[] autoProgressLines;
+    public float autoprogressDelay=1.5f;
+}
+
+   
+
