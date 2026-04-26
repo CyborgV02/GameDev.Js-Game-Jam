6e86f1948171e4fc9011a341e74f895ae4eea04e
Cyborg <ahmedkamalv2@gmail.com>
2026-04-26 03:03:59 +0200

    Implemented TYR animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Sprite[] sprites;
     private Rigidbody2D playerRb;
     private Vector2 moveInput;
-    private float moveSpeed = 5.0f;
+    private float moveSpeed = 2.5f;
     [SerializeField] private MainCharacter playerCharacter;
     public MainCharacter Character { get { return playerCharacter; } }
+    public Animator anim;
     
 
     void Awake()
@@ -18,14 +19,19 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody2D>();
         playerCharacter = new MainCharacter(sprites); // Pass the sprites array here
         InputController.OnMove += Move;
+        anim=GetComponent<Animator>();
     }
 
     void Update()
     {
         playerRb.velocity = moveInput * moveSpeed;
+
     }
-    public void Move(Vector2 context)
-    {
-        moveInput = context;
-    }
+   public void Move(Vector2 context)
+   {
+    moveInput = context;
+    anim.SetBool("Iswalking", moveInput != Vector2.zero);
+    anim.SetFloat("InputX", moveInput.x);
+    anim.SetFloat("InputY", moveInput.y);
+   }
 }
